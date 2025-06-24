135dc9b2c61cf5b2369aa8aad808a0d0205aa57d
OU-Studio <151074387+OU-Studio@users.noreply.github.com>
2025-06-24 08:14:51 +0100

    Update allowedSites.js

--- config/allowedSites.js
+++ config/allowedSites.js
@@ -1,5 +1,6 @@
 // config/allowedSites.js
 module.exports = {
   "managemywebsite.com": "sk_mmw_1234",
-  "dixparkconservancy.org": "sk_dix_1234"
+  "dixparkconservancy.org": "sk_dix_1234",
+  "northlondonskateclub.com": "sk_nlsc_1234"
 };
